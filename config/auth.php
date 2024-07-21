00194fed05dda882431b75de04025da1939870c5
tw1sst <likovdenis12@gmail.com>
2024-07-21 20:09:56 +0300

    process

--- config/auth.php
+++ config/auth.php
@@ -13,9 +13,21 @@ return [
     |
     */
 
+//     'defaults' => [
+//         'guard' => env('AUTH_GUARD', 'web'),
+//         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+//     ],
+
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => 'api',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
     ],
 
     /*
@@ -35,12 +47,12 @@ return [
     |
     */
 
-    'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-    ],
+//     'guards' => [
+//         'web' => [
+//             'driver' => 'session',
+//             'provider' => 'users',
+//         ],
+//     ],
 
     /*
     |--------------------------------------------------------------------------
--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,29 @@ use App\Http\Controllers\Api\V1\PostController;
 use App\Http\Controllers\Api\V1\SchoolController;
 use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\Api\V1\CourseController;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+Route::post('login', [AuthController::class, 'login']);
 
-Route::resource('user', UserController::class);
-Route::resource('lesson', LessonController::class);
-Route::resource('post', PostController::class);
-Route::resource('school', SchoolController::class);
-Route::resource('task', TaskController::class);
-Route::resource('course', CourseController::class);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+
+   // Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+    // Route::resource('auth', AuthController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('lesson', LessonController::class);
+    Route::resource('post', PostController::class);
+    Route::resource('school', SchoolController::class);
+    Route::resource('task', TaskController::class);
+    Route::resource('course', CourseController::class);
+});
